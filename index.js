3b22941ee6a3d29418ac7a553e51eb973bf6589b
Humberto CL <87952396+Arokeji@users.noreply.github.com>
2023-05-02 13:01:36 +0200

    Prueba de deploy en Vercel con json y el await del conect para que no falle

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 const express = require("express");
 const { userRoutes } = require("./routes/book.routes.js");
 
-// Conexion a la BBDD
-const { connect } = require("./db.js");
-try {
-  await connect();
-} catch (error) {
-  console.log(`Ha ocurrido un error al conectar: ${error}`);
-}
+// La intencion del main es que sea una funcion async para poder hacer await en connect
+// para el despliegue en Vercel
+const main = async () => {
+  // Conexion a la BBDD
+  const { connect } = require("./db.js");
+  try {
+    await connect();
+  } catch (error) {
+    console.log(`Ha ocurrido un error al conectar: ${error}`);
+  }
 
-// Configuracion del servidor
-const PORT = 3000;
-const server = express();
-server.use(express.json());
-server.use(express.urlencoded({ extended: false }));
+  // Configuracion del servidor
+  const PORT = 3000;
+  const server = express();
+  server.use(express.json());
+  server.use(express.urlencoded({ extended: false }));
 
-// Rutas
-const router = express.Router();
-router.get("/", (req, res) => {
-  res.send("Library API");
-});
-router.get("*", (req, res) => {
-  res.status(404).send("La pagina solicitada no existe");
-})
+  // Rutas
+  const router = express.Router();
+  router.get("/", (req, res) => {
+    res.send("Library API");
+  });
+  router.get("*", (req, res) => {
+    res.status(404).send("La pagina solicitada no existe");
+  });
 
-// Uso del router
-server.use("/book", userRoutes);
-server.use("/", router);
+  // Uso del router
+  server.use("/book", userRoutes);
+  server.use("/", router);
 
-// Ejecución del servidor
-server.listen(PORT, () => {
-  console.log(`Servidor funcionando en puerto ${PORT}`);
-});
+  // Ejecución del servidor
+  server.listen(PORT, () => {
+    console.log(`Servidor funcionando en puerto ${PORT}`);
+  });
+};
+
+main();
